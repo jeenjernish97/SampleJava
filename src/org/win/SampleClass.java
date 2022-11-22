ab5af381cf9161699130538116078a1f9e4fd8f1
jeenjernish97 <jeenjernish@gmail.com>
2022-11-22 21:15:31 +0530

    B branch is updated

--- src/org/win/SampleClass.java
+++ src/org/win/SampleClass.java
@@ -5,15 +5,15 @@ public class SampleClass {
 	public static void main(String[] args) {
 		String s = "java project";
 				System.out.println(s.indexOf(3));
-		
-<<<<<<< HEAD
+				int a =10;
+				System.out.println(a);
+				System.out.println(1234567);
+
 		String s1[]= {"java","python","selenium"};
 		System.out.println(s1[2]);
-=======
-		int a =10;
-		System.out.println(a);
-		System.out.println(1234567);
->>>>>>> bc753b3c392895c4ff7351b78c1ff5b670c39a66
+
+	
+
 	}
 	
 }
